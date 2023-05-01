6988db191c606d937031031f3f5ba60d56792631
Joel Binnquist <joel.binnquist@gmail.com>
2023-05-01 20:52:26 +0200

    Fixa catalog-service props

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,12 @@ group = "com.polarbookshop"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
 repositories {
     mavenCentral()
 }
@@ -20,14 +26,15 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.restdocs:spring-restdocs-webtestclient:3.0.0")
+	implementation("org.projectlombok:lombok")
 
 	asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor:3.0.0")
-    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc:3.0.0")
-    implementation("org.springframework.restdocs:spring-restdocs-webtestclient:3.0.0")
-
-	implementation("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc:3.0.0")
     testAnnotationProcessor("org.projectlombok:lombok")
 }
 
--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,12 +1,17 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HomeController {
+  private final PolarProperties polarProperties;
+
   @GetMapping("/")
   public String home() {
-    return "Welcome to the Book Catalog Service";
+    return polarProperties.getGreeting();
   }
 }
