903e62200c348de3fccbe92a53a98d842d7ca07f
Joel Binnquist <joel.binnquist@gmail.com>
2023-06-08 22:34:35 +0200

    Kubernetes

---
+++ Tiltfile
@@ -0,0 +1,14 @@
+# Build
+custom_build(
+    # Name of the container image
+    ref = 'catalog-service',
+    # Command to build the container image
+    command = './gradlew bootBuildImage --imageName $EXPECTED_REF',
+    # Files to watch that trigger a new build
+    deps = ['build.gradle', 'src']
+)
+# Deploy
+k8s_yaml(['k8s/deployment.yml', 'k8s/service.yml'])
+k8s_yaml(['../deployment/kubernetes/platform/development/services/postgresql.yml'])
+# Manage
+k8s_resource('catalog-service', port_forwards=['9001'])
--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,13 +1,22 @@
 package com.polarbookshop.catalogservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("integration-test")
 class CatalogServiceApplicationTests {
 
+	@Autowired
+	ApplicationContext context;
 	@Test
 	void contextLoads() {
+		assertThat(context).isNotNull();
 	}
 
 }
